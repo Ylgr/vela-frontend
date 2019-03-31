76aaae278b16993c6284bcd4b61c72d5a53a73f6
Ylgr <quyen.luminous@gmail.com>
2019-03-31 11:43:48 +0700

    detail

--- src/components/advert/AdDetails.js
+++ src/components/advert/AdDetails.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import connect from "react-redux/es/connect/connect";
 import PropTypes from "prop-types";
-
+import {
+    Card,
+    CardBody,
+    CardImg,
+    CardText,
+    CardTitle,
+    Col,
+} from 'reactstrap';
 class AdDetails extends React.Component {
     render() {
         return(
             <div>
                 {this.props.adReports.map( (ad,index) =>
 
-                    <div key = {index}>
-                        <p>Ad name: {ad.name}</p>
-                        <p>Ad data: {ad.data}</p>
-                        <p>Clicked: {ad.click}</p>
-                    </div>
+                    <Col md={6} sm={6} xs={12} className="mb-3" key={index}>
+                    <Card className="flex-row">
+                    <CardBody>
+                    <CardTitle>{ad.name}</CardTitle>
+                    <CardText>
+                        Category: {ad.category}<br/>
+                        Click: {ad.click}<br/>
+                        View: {ad.view}
+                    </CardText>
+                    </CardBody>
+                    <CardImg
+                    className="card-img-left"
+                    src={ad.data}
+                    style={{ width: 'auto', height: 150 }}
+                    />
+                    </Card>
+                    </Col>
                 )}
             </div>
         )}
--- src/components/layout/TopBar.js
+++ src/components/layout/TopBar.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 
 class TopBar extends React.Component{
     render() {
         return(
             <nav className="cr-header bg-white navbar navbar-expand navbar-light">
+                <ul className="cr-header__nav-right navbar-nav">
+                    <li><p>Hello {this.props.advertiser.name}!</p></li>
+                </ul>
             </nav>
         )
     }
 }
-export default TopBar;
+
+TopBar.propTypes = {
+    gasWallet: PropTypes.object.isRequired,
+    advertiser: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+    return {
+        advertiser: state.advertiser,
+        gasWallet: state.gasWallet
+    };
+}
+
+export default connect(mapStateToProps)(TopBar);
