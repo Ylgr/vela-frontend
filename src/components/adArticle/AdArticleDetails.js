51c4f6aceb841979146cab8f8424ea66db4106a3
Ylgr <quyen.luminous@gmail.com>
2019-04-14 22:49:14 +0700

    add share icon

--- src/components/adArticle/AdArticleDetails.js
+++ src/components/adArticle/AdArticleDetails.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Card, CardBody, CardImg, CardText, CardTitle, Col, Row} from "reactstrap";
-
+import {MdShare} from 'react-icons/md';
 class AdArticleDetails extends React.Component {
 
     render() {
         return(
+            <a href={this.props.article.url} target="_blank">
             <Col>
                 <Card>
                     <CardImg top src={this.props.article.data} />
@@ -18,10 +19,15 @@ class AdArticleDetails extends React.Component {
                                 </CardText>
                             </CardBody>
                         </Col>
-                        <Col md="2" sm="2" xs="2"><p>Share this</p></Col>
+                        <Col md="2" sm="2" xs="2">
+                            <a href="share">
+                                <MdShare/>
+                            </a>
+                        </Col>
                     </Row>
                 </Card>
             </Col>
+            </a>
         )
     }
 }
